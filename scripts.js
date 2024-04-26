694f967628bdcc076ef523eb2a99496edb8642d9
feliperyo <ryofeliperyo@gmail.com>
2024-04-26 12:55:20 -0300

    fazendo horário e cronômetro funcionarem

--- scripts.js
+++ scripts.js
@@ -10,6 +10,7 @@ elemsExercicio.forEach((elemExercicio) => {
 
 elemVoltar.addEventListener('click', () => {
     elemTela.classList.remove('tela--cronometro')
+    clicouStop()
 })
 
 const elemStop = document.querySelector('.close')
@@ -17,14 +18,65 @@ const elemPlay = document.querySelector('.play')
 const elemPause = document.querySelector('.pause')
 const elemCronometro = document.querySelector('.cronometro')
 
-elemPlay.addEventListener('click', () => {
+let timer = 0
+let idIntervalo
+
+function clickPlay() {
     elemCronometro.classList.add('iniciou')
-})
 
-elemStop.addEventListener('click', () => {
-    elemCronometro.classList.remove('iniciou')
-})
+    rodarTimer()
+}
 
-elemPause.addEventListener('click', () => {
+elemPlay.addEventListener('click', clickPlay)
+
+function clicouStop() {
     elemCronometro.classList.remove('iniciou')
-})
+
+    clearInterval(idIntervalo)
+    timer = 0
+    attCronometro()
+}
+
+elemStop.addEventListener('click', clicouStop)
+
+function clicouPause() {
+    elemCronometro.classList.remove('iniciou')
+
+    clearInterval(idIntervalo)
+}
+
+elemPause.addEventListener('click', clicouPause)
+
+const elemHorario = document.querySelector('.horario')
+
+function attHorario() {
+    const horas = new Date().getHours().toString().padStart(2, '0')
+    const minutos = new Date().getMinutes().toString().padStart(2, '0')
+
+    elemHorario.innerText = horas + ':' + minutos
+}
+
+setInterval(() => {
+    attHorario()
+}, 1000);
+
+function rodarTimer() {
+    idIntervalo = setInterval(() => {
+        timer = timer + 10
+        attCronometro()
+    }, 10);
+}
+
+const elemMinuto = document.querySelector('.minuto')
+const elemSegundo = document.querySelector('.segundo')
+const elemCentesimo = document.querySelector('.centesimo')
+
+function attCronometro() {
+    const minutos = Math.floor(timer / 60000).toString().padStart(2, '0')
+    const segundos = Math.floor((timer % 60000) / 1000).toString().padStart(2, '0')
+    const centesimos = Math.floor(((timer % 60000) % 1000) / 10).toString().padStart(2, '0')
+
+    elemMinuto.innerHTML = minutos
+    elemSegundo.innerHTML = segundos
+    elemCentesimo.innerHTML = centesimos
+}
